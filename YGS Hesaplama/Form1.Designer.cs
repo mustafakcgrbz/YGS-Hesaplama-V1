a4bf2d151e7b158b8284c7c1f80089b2fa86cbd4
Mustafa <mustafakcgrbz@gmail.com>
2017-05-02 12:00:51 +0300

    Add project files.

---
+++ YGS Hesaplama/Form1.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace YGS_Hesaplama
+{
+    partial class FrmYGS
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtTurkce = new System.Windows.Forms.TextBox();
+            this.TxtSosyal = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMatematik = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtFen = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnHesapla = new System.Windows.Forms.Button();
+            this.LblYgs1 = new System.Windows.Forms.Label();
+            this.LblYgs2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LblYgs3 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.LblYgs4 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.LblYgs5 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.LblYgs6 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(13, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Türkçe :";
+            // 
+            // TxtTurkce
+            // 
+            this.TxtTurkce.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.TxtTurkce.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtTurkce.Location = new System.Drawing.Point(123, 14);
+            this.TxtTurkce.Name = "TxtTurkce";
+            this.TxtTurkce.Size = new System.Drawing.Size(86, 26);
+            this.TxtTurkce.TabIndex = 1;
+            this.TxtTurkce.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // TxtSosyal
+            // 
+            this.TxtSosyal.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.TxtSosyal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtSosyal.Location = new System.Drawing.Point(352, 14);
+            this.TxtSosyal.Name = "TxtSosyal";
+            this.TxtSosyal.Size = new System.Drawing.Size(86, 26);
+            this.TxtSosyal.TabIndex = 3;
+            this.TxtSosyal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(242, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sosyal :";
+            // 
+            // TxtMatematik
+            // 
+            this.TxtMatematik.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.TxtMatematik.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtMatematik.Location = new System.Drawing.Point(123, 57);
+            this.TxtMatematik.Name = "TxtMatematik";
+            this.TxtMatematik.Size = new System.Drawing.Size(86, 26);
+            this.TxtMatematik.TabIndex = 5;
+            this.TxtMatematik.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(13, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Matematik :";
+            // 
+            // TxtFen
+            // 
+            this.TxtFen.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.TxtFen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TxtFen.Location = new System.Drawing.Point(352, 57);
+            this.TxtFen.Name = "TxtFen";
+            this.TxtFen.Size = new System.Drawing.Size(86, 26);
+            this.TxtFen.TabIndex = 7;
+            this.TxtFen.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(242, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Fen :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(13, 175);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "YGS 1 :";
+            // 
+            // BtnHesapla
+            // 
+            this.BtnHesapla.BackColor = System.Drawing.Color.DarkSalmon;
+            this.BtnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.BtnHesapla.Location = new System.Drawing.Point(161, 106);
+            this.BtnHesapla.Name = "BtnHesapla";
+            this.BtnHesapla.Size = new System.Drawing.Size(135, 43);
+            this.BtnHesapla.TabIndex = 9;
+            this.BtnHesapla.Text = "Hesapla";
+            this.BtnHesapla.UseVisualStyleBackColor = false;
+            this.BtnHesapla.Click += new System.EventHandler(this.BtnHesapla_Click);
+            // 
+            // LblYgs1
+            // 
+            this.LblYgs1.AutoSize = true;
+            this.LblYgs1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LblYgs1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblYgs1.Location = new System.Drawing.Point(110, 175);
+            this.LblYgs1.Name = "LblYgs1";
+            this.LblYgs1.Size = new System.Drawing.Size(20, 24);
+            this.LblYgs1.TabIndex = 10;
+            this.LblYgs1.Text = "0";
+            // 
+            // LblYgs2
+            // 
+            this.LblYgs2.AutoSize = true;
+            this.LblYgs2.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LblYgs2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblYgs2.Location = new System.Drawing.Point(339, 175);
+            this.LblYgs2.Name = "LblYgs2";
+            this.LblYgs2.Size = new System.Drawing.Size(20, 24);
+            this.LblYgs2.TabIndex = 12;
+            this.LblYgs2.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(242, 175);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(73, 24);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "YGS 2 :";
+            // 
+            // LblYgs3
+            // 
+            this.LblYgs3.AutoSize = true;
+            this.LblYgs3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LblYgs3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblYgs3.Location = new System.Drawing.Point(110, 231);
+            this.LblYgs3.Name = "LblYgs3";
+            this.LblYgs3.Size = new System.Drawing.Size(20, 24);
+            this.LblYgs3.TabIndex = 14;
+            this.LblYgs3.Text = "0";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(13, 231);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(73, 24);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "YGS 3 :";
+            // 
+            // LblYgs4
+            // 
+            this.LblYgs4.AutoSize = true;
+            this.LblYgs4.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LblYgs4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblYgs4.Location = new System.Drawing.Point(339, 231);
+            this.LblYgs4.Name = "LblYgs4";
+            this.LblYgs4.Size = new System.Drawing.Size(20, 24);
+            this.LblYgs4.TabIndex = 16;
+            this.LblYgs4.Text = "0";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(242, 231);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(73, 24);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "YGS 4 :";
+            // 
+            // LblYgs5
+            // 
+            this.LblYgs5.AutoSize = true;
+            this.LblYgs5.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LblYgs5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblYgs5.Location = new System.Drawing.Point(110, 281);
+            this.LblYgs5.Name = "LblYgs5";
+            this.LblYgs5.Size = new System.Drawing.Size(20, 24);
+            this.LblYgs5.TabIndex = 18;
+            this.LblYgs5.Text = "0";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label13.Location = new System.Drawing.Point(13, 281);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(73, 24);
+            this.label13.TabIndex = 17;
+            this.label13.Text = "YGS 5 :";
+            // 
+            // LblYgs6
+            // 
+            this.LblYgs6.AutoSize = true;
+            this.LblYgs6.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.LblYgs6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.LblYgs6.Location = new System.Drawing.Point(339, 281);
+            this.LblYgs6.Name = "LblYgs6";
+            this.LblYgs6.Size = new System.Drawing.Size(20, 24);
+            this.LblYgs6.TabIndex = 20;
+            this.LblYgs6.Text = "0";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label15.Location = new System.Drawing.Point(242, 281);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(73, 24);
+            this.label15.TabIndex = 19;
+            this.label15.Text = "YGS 6 :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label6.Location = new System.Drawing.Point(129, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(185, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Mustafa KOCAGÜRBÜZ";
+            // 
+            // FrmYGS
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(454, 346);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.LblYgs6);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.LblYgs5);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.LblYgs4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.LblYgs3);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.LblYgs2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.LblYgs1);
+            this.Controls.Add(this.BtnHesapla);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtFen);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtMatematik);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSosyal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtTurkce);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmYGS";
+            this.Text = "YGS Puan Hesaplama";
+            this.Load += new System.EventHandler(this.FrmYGS_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtTurkce;
+        private System.Windows.Forms.TextBox TxtSosyal;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMatematik;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtFen;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnHesapla;
+        private System.Windows.Forms.Label LblYgs1;
+        private System.Windows.Forms.Label LblYgs2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label LblYgs3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label LblYgs4;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label LblYgs5;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label LblYgs6;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ YGS Hesaplama/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YGS_Hesaplama
+{
+    public partial class FrmYGS : Form
+    {
+        public FrmYGS()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnHesapla_Click(object sender, EventArgs e)
+        {
+            Double turkce, sosyal, matematik, fen;
+            Double ygs1, ygs2, ygs3, ygs4, ygs5, ygs6;
+            if (TxtTurkce.Text != "")
+                turkce = Convert.ToDouble(TxtTurkce.Text);
+            else
+                turkce = 0.0;
+            if (TxtSosyal.Text != "")
+                sosyal = Convert.ToDouble(TxtSosyal.Text);
+            else
+                sosyal = 0.0;
+            if (TxtMatematik.Text != "")
+                matematik = Convert.ToDouble(TxtMatematik.Text);
+            else
+                matematik = 0.0;
+            if (TxtFen.Text != "")
+                fen = Convert.ToDouble(TxtFen.Text);
+            else
+                fen = 0.0;
+            if (turkce > 40 | matematik > 40 | sosyal > 40 | fen > 40)
+                MessageBox.Show("Lütfen 40' ın üzerinde net sayısı girmeyiniz");
+            else
+            {
+                ygs1 = 100.160 + (turkce * 1.999) + (sosyal * 1) + (matematik * 3.998) + (fen * 2.999);
+                LblYgs1.Text = ygs1.ToString();
+                ygs2 = 100.160 + (turkce * 1.999) + (sosyal * 1) + (matematik * 2.999) + (fen * 3.998);
+                LblYgs2.Text = ygs2.ToString();
+                ygs3 = 100.160 + (turkce * 3.998) + (sosyal * 2.999) + (matematik * 1.999) + (fen * 1);
+                LblYgs3.Text = ygs3.ToString();
+                ygs4 = 100.160 + (turkce * 2.999) + (sosyal * 3.998) + (matematik * 1.999) + (fen * 1);
+                LblYgs4.Text = ygs4.ToString();
+                ygs5 = 100.120 + (turkce * 3.699) + (sosyal * 1.999) + (matematik * 3.299) + (fen * 1);
+                LblYgs5.Text = ygs5.ToString();
+                ygs6 = 100.120 + (turkce * 3.299) + (sosyal * 1) + (matematik * 3.699) + (fen * 1.999);
+                LblYgs6.Text = ygs6.ToString();
+            }
+        }
+
+        private void FrmYGS_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
